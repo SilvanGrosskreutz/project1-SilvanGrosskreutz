8ada7bbb319ecfcd26ba8fddcde1148164e23a1a
SilvanGrosskreutz <silvan.grosskreutz@htp-tel.de>
2022-04-25 15:11:37 +0200

    25.4. Part 2

--- project1-SilvanGrosskreutz/src/main/java/com/revature/controllers/MenuController.java
+++ project1-SilvanGrosskreutz/src/main/java/com/revature/controllers/MenuController.java
@@ -14,13 +14,23 @@ public class MenuController {
 		
 		printHomeMenu();
 		
+		String response = scan.nextLine();
+		
+		while(response != "0") {
+			
+		}
+		
 	}
 
 
 	private void printHomeMenu() {
 		System.out.println("What do you want to do?" +
 				"1) See all Users. \n" +
-				"2) See one User. \n");
+				"2) See one User. \n" +
+				"3) Add one User. \n" +
+				"4) Update one User. \n" +
+				"5) Delete one User. \n" +
+				"0) Close the Application.");
 		
 	}
 	
